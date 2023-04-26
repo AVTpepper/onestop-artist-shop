4f34b974b9d111ee6006f72aa0136ccdeddcd290
AVTpepper <Alexander.tastad@hotmail.com>
2023-04-26 13:43:40 +0000

    Add: shopping cart user functionality and responsiveness

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_shopping_cart, name='shopping_cart'),
     path('add/<int:artwork_id>/', views.add_to_cart, name='add_to_cart'),
+    path('update_cart/', views.update_cart, name='update_cart'),
+    path('shopping_cart/delete_cart_item/', views.delete_cart_item, name='delete_cart_item'),
 ]
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.http import HttpResponse
+
 from shopping_cart.forms import AddToCartForm
 from artworks.models import Artwork
 from .models import CartItem
@@ -10,6 +12,7 @@ def view_shopping_cart(request):
     cart = request.session.get('cart', {})
 
     cart_items = []
+    grand_total = 0 
     for artwork_id, quantity in cart.items():
         artwork = get_object_or_404(Artwork, id=artwork_id)
 
@@ -19,13 +22,16 @@ def view_shopping_cart(request):
         else:
             quantity_value = quantity
         
+        subtotal = artwork.price * quantity_value
+        grand_total += subtotal  
+
         cart_items.append({
             'artwork': artwork,
             'quantity': quantity_value,
-            'subtotal': artwork.price * quantity_value,
+            'subtotal': subtotal,
         })
 
-    context = {'cart_items': cart_items}
+    context = {'cart_items': cart_items, 'grand_total': grand_total} 
     return render(request, 'shopping_cart/cart.html', context)
 
 
@@ -49,3 +55,38 @@ def add_to_cart(request, artwork_id):
             return redirect('shopping_cart')
 
     return redirect('home')
+
+
+def update_cart(request):
+    if request.method == 'POST':
+        item_id = request.POST.get('item_id')
+        quantity = request.POST.get('quantity')
+        
+        # Update the cart item quantity directly in the view function
+        cart = request.session.get('cart', {})
+        cart[item_id] = int(quantity)
+
+        # Save the updated cart to the session
+        request.session['cart'] = cart
+        request.session.modified = True
+
+        return redirect('shopping_cart')
+    else:
+        return redirect('shopping_cart')
+
+
+def delete_cart_item(request):
+    if request.method == "POST":
+        item_id = request.POST.get('item_id')
+        
+        # Remove the cart item directly in the view function
+        cart = request.session.get('cart', {})
+        if item_id in cart:
+            del cart[item_id]
+        request.session['cart'] = cart
+        
+        return redirect('shopping_cart')
+    else:
+        return redirect('shopping_cart')
+
+
