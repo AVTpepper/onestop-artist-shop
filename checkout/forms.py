3bcfa3b5dcd48f73fe3bde11c57b72074a7ad038
AVTpepper <Alexander.tastad@hotmail.com>
2023-04-28 06:56:11 +0000

    Update: form attributes

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,5 +1,3 @@
-# checkout/forms.py
-
 from django import forms
 from .models import Order
 
@@ -7,3 +5,29 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('user', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'user': 'User',
+            'full_name': 'Full Name',
+            'email': 'Email',
+            'phone_number': 'Phone Number',
+            'postcode': 'Postcode',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County, State or Locality',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].label = False
