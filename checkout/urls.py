50881b6498811401057a8d7c498dfd6e49cd8297
AVTpepper <Alexander.tastad@hotmail.com>
2023-04-28 07:45:47 +0000

    Add: complete order url

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('complete_order/', views.complete_order, name='complete_order'),
 ]
