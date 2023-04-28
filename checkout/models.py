69c551c2e9d4146282622680dcec449f15ce1669
AVTpepper <Alexander.tastad@hotmail.com>
2023-04-28 06:57:18 +0000

    Update: model fields and methods

--- checkout/models.py
+++ checkout/models.py
@@ -1,25 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import User
 from artworks.models import Artwork
+from django_countries.fields import CountryField
+
+
+from django.db.models import Sum
+import uuid
+
 
 
 class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     date_ordered = models.DateTimeField(auto_now_add=True)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
-    full_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
-    country = models.CharField(max_length=40)
-    postcode = models.CharField(max_length=20)
-    town_or_city = models.CharField(max_length=40)
-    street_address1 = models.CharField(max_length=40)
-    street_address2 = models.CharField(max_length=40)
-    county = models.CharField(max_length=80)
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added, accounting for delivery costs.
+        """
+        self.total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'Order {self.id}'
+        return self.order_number
 
 
 
