a8fbbc5205f4dc3898535e002e70f10f73b63309
AVTpepper <Alexander.tastad@hotmail.com>
2023-04-29 20:41:06 +0000

    Update: URL pattern for updated view

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('complete_order/', views.complete_order, name='complete_order'),
+    path('complete_order/<int:order_id>/', views.complete_order, name='complete_order'),
 ]
