f533ef3cd48ab5919a4e5fdfa25bc6917db05612
AVTpepper <Alexander.tastad@hotmail.com>
2023-04-30 16:29:10 +0000

    Add: new complete_order, webhook and cache_checkout_data URL

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('complete_order/<int:order_id>/', views.complete_order, name='complete_order'),
+    path('complete_order/<order_number>/', views.complete_order, name='complete_order'),
+    path('wh/', webhook, name='webhook'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
 ]
