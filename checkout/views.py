101845586b326b855c78b7c66745f89789062d7c
AVTpepper <Alexander.tastad@hotmail.com>
2023-04-30 16:30:38 +0000

    Add + Update: cache_checkout_data function is new, updated error handling and post requests for checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,15 @@
 from django.conf import settings
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.urls import reverse
 from .forms import OrderForm
 from .models import OrderLineItem, Order
+from artworks.models import Artwork
 from shopping_cart.contexts import shopping_cart_contents
 
 import stripe
+import json
 
 
 def checkout(request):
@@ -23,24 +27,50 @@ def checkout(request):
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY,
     )
-    print(intent)
 
     if request.method == 'POST':
-        form = OrderForm(request.POST)
+        shopping_cart = request.session.get('shopping_cart', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        form = OrderForm(form_data)
         if form.is_valid():
             order = form.save(commit=False)
             order.user = request.user if request.user.is_authenticated else None
             order.total = total
             order.save()
             for item in current_cart['cart_items']:
-                OrderLineItem.objects.create(
-                    order=order,
-                    artwork=item['artwork'],
-                    quantity=item['quantity'],
-                    lineitem_total=item['subtotal']
-                )
+                print(item)
+                try:
+                    artwork = item['artwork']
+                    quantity = item['quantity']
+                    lineitem_total = item['subtotal']
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        artwork=artwork,
+                        quantity=quantity,
+                        lineitem_total=lineitem_total
+                    )
+                    order_line_item.save()
+                except Artwork.DoesNotExist:
+                    messages.error(request, (
+                        "One of the artworks in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('shopping_cart'))
+
             messages.success(request, 'Order successfully placed!')
-            return redirect('complete_order', order_id=order.id)
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('complete_order', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. Please check your information.')
     else:
@@ -57,7 +87,44 @@ def checkout(request):
     return render(request, 'checkout/checkout.html', context)
 
 
-def complete_order(request, order_id):
-    order = get_object_or_404(Order, id=order_id)
-    context = {'order': order}
-    return render(request, 'checkout/complete_order.html', context)
+
+
+def complete_order(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+
+    if 'shopping_cart' in request.session:
+        del request.session['shopping_cart']
+
+
+    template = 'checkout/complete_order.html'
+    context = {
+        'order': order,
+    }
+
+
+    return render(request, template, context)
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'shopping_cart': json.dumps(request.session.get('shopping_cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
