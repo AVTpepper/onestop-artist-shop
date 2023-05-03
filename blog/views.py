17b72a83175f269ec0d4475821c36a4502aade51
AVTpepper <Alexander.tastad@hotmail.com>
2023-05-03 10:29:59 +0000

    Add: blog functionality views such as, like, comment, delete

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post, Comment
+from django.db.models import Count
 from django.contrib.auth.decorators import login_required
+
+from .models import Post, Comment, Like
 from .forms import CommentForm, PostForm
 
 
+
 def post_list(request):
-    posts = Post.objects.order_by('-created_at')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.all().annotate(
+        likes_count=Count('likes'),
+        comments_count=Count('comments')
+    )
+
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post_list.html', context)
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    liked = Like.objects.filter(user=request.user, post=post).exists()
+
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        comment_form = CommentForm()
+
+    context = {
+        'post': post,
+        'liked': liked,
+        'comment_form': comment_form,
+    }
+    return render(request, 'blog/post_detail.html', context)
+
 
 @login_required
 def add_comment(request, pk):
@@ -25,20 +55,13 @@ def add_comment(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/add_comment.html', {'form': form})
+    
+    context = {
+        'form': form
+    }
 
-@login_required
-def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
+    return render(request, 'blog/add_comment.html', context)
 
-@login_required
-def comment_remove(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    post_pk = comment.post.pk
-    comment.delete()
-    return redirect('post_detail', pk=post_pk)
 
 def post_create(request):
     if request.method == 'POST':
@@ -50,4 +73,52 @@ def post_create(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_form.html', {'form': form})
+    
+    context = {
+        'form': form
+    }
+
+    return render(request, 'blog/post_create.html', context)
+
+
+@login_required
+def like_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    liked = Like.objects.filter(user=request.user, post=post)
+    if liked.exists():
+        liked.delete()
+    else:
+        Like.objects.create(user=request.user, post=post)
+    return redirect('post_detail', pk=post.pk)
+
+
+@login_required
+def comment_edit(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post_pk)
+
+    form = CommentForm(instance=comment)
+
+    context = {
+        'form': form,
+        'comment': comment
+    }
+    return redirect('post_detail', pk=post_pk)
+
+
+@login_required
+def comment_delete(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('post_detail', pk=post_pk)
+
+    return redirect('post_detail', pk=post_pk)
