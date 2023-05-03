dd3113c2996f49ec571565e33ec90b45455a5946
AVTpepper <Alexander.tastad@hotmail.com>
2023-05-03 10:28:49 +0000

    Update: like, edit, delete and create urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/comment/', views.add_comment, name='add_comment'),
-    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
-    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('comment/<int:pk>/edit/', views.comment_edit, name='comment_edit'),
+    path('comment/<int:pk>/delete/', views.comment_delete, name='comment_delete'),
+    path('post/create/', views.post_create, name='post_create'),
+    path('like/<int:post_id>/', views.like_post, name='like_post'),
 ]
