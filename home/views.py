cfe2be9fde0ed32f03fac020c578a2c46506b14c
AVTpepper <Alexander.tastad@hotmail.com>
2023-05-03 11:02:24 +0000

    Add: about view

--- home/views.py
+++ home/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'home/about.html')
