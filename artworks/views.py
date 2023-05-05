fb6f74cec24f215016cd01cfd75faa099d26555e
AVTpepper <Alexander.tastad@hotmail.com>
2023-05-05 04:40:01 +0000

    Add: edit and delete artwork views

--- artworks/views.py
+++ artworks/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import user_passes_test
 
 from .models import Artwork, Category
 from shopping_cart.forms import AddToCartForm
+from .forms import ArtworkEditForm
 
 
 # Create your views here.
@@ -32,3 +34,29 @@ def artwork_detail(request, artwork_id):
     context = {'artwork': artwork, 'form': form}
 
     return render(request, 'artworks/artwork_detail.html', context)
+
+def is_staff(user):
+    return user.is_staff
+
+
+@user_passes_test(is_staff)
+def edit_artwork(request, artwork_id):
+    artwork = get_object_or_404(Artwork, id=artwork_id)
+    if request.method == 'POST':
+        form = ArtworkEditForm(request.POST, request.FILES, instance=artwork)
+        if form.is_valid():
+            form.save()
+            return redirect('artwork_detail', artwork_id=artwork.id)
+    else:
+        form = ArtworkEditForm(instance=artwork)
+    
+    context = {'artwork': artwork, 'form': form}
+
+    return render(request, 'artworks/edit_artwork.html')
+
+
+@user_passes_test(is_staff)
+def delete_artwork(request, artwork_id):
+    artwork = get_object_or_404(Artwork, id=artwork_id)
+    artwork.delete()
+    return redirect('artworks')
