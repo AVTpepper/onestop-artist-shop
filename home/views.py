06b96d366e360138691cc4fd56255fd5806a4294
AVTpepper <Alexander.tastad@hotmail.com>
2023-05-06 17:10:25 +0000

    Update: home view

--- home/views.py
+++ home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from artworks.models import Artwork
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    featured_artworks = Artwork.objects.filter(is_featured=True).order_by('?')[:3]
+    request.session['referrer'] = 'home'
+
+    context = {
+        'featured_artworks': featured_artworks
+    }
+
+    return render(request, 'home/index.html', context)
+
 
 def about(request):
     return render(request, 'home/about.html')
