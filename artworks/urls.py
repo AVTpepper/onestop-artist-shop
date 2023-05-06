426b95fd25e99c02e76fdca6f8a53e50274eb679
AVTpepper <Alexander.tastad@hotmail.com>
2023-05-06 16:54:43 +0000

    Add: error templates

--- artworks/urls.py
+++ artworks/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_artworks, name='artworks'),
-    path('artwork/<int:artwork_id>/', views.artwork_detail, name='artwork_detail'),
+    path('artwork_detail/<int:artwork_id>/', views.artwork_detail, name='artwork_detail'),
     path('edit/<int:artwork_id>/', views.edit_artwork, name='edit_artwork'),
     path('delete/<int:artwork_id>/', views.delete_artwork, name='delete_artwork'),
 ]
