7892973a0defff08320d834d57237c16ad978598
AVTpepper <Alexander.tastad@hotmail.com>
2023-05-06 18:05:49 +0000

    Update: removed secret key and set debug

--- onestop_artist_shop/settings.py
+++ onestop_artist_shop/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--o7ajiqws1b9(hc(ilk22qr+mdf#1q@n86i*nv%jmtudq(!*8^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-avtpepper-onestop-artist-rmkk28poe3.us2.codeanyapp.com',
