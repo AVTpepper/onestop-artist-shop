5629795b614126385950ce372fa60afee44b59c3
AVTpepper <Alexander.tastad@hotmail.com>
2023-05-06 17:26:48 +0000

    Add: artwork management path

--- artworks/urls.py
+++ artworks/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('artwork_detail/<int:artwork_id>/', views.artwork_detail, name='artwork_detail'),
     path('edit/<int:artwork_id>/', views.edit_artwork, name='edit_artwork'),
     path('delete/<int:artwork_id>/', views.delete_artwork, name='delete_artwork'),
+    path('artwork-management/', views.artwork_management, name='artwork_management'),
+    path('add-artwork/', views.add_artwork, name='add_artwork'),
 ]
 
