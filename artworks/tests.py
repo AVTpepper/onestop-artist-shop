263292fa6bb04c81597cc41a3c787ea40feb5b18
AVTpepper <Alexander.tastad@hotmail.com>
2023-05-07 10:09:01 +0000

    Add: trying automatic tests

--- artworks/tests.py
+++ artworks/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Artwork
 
 # Create your tests here.
+
+class ArtworkModelTests(TestCase):
+
+    def test_artwork_creation(self):
+        """
+        Test the creation of an Artwork instance.
+        """
+        artwork = Artwork.objects.create(name="Test Artwork", description="Test description", price=10)
+        self.assertEqual(artwork.name, "Test Artwork")
+        self.assertEqual(artwork.description, "Test description")
+        self.assertEqual(artwork.price, 10)
+
+class ArtworkViewTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.artwork = Artwork.objects.create(name="Test Artwork", description="Test description", price=10)
+
+    def test_view_artwork_detail(self):
+        """
+        Test the artwork detail view.
+        """
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('artwork_detail', args=[str(self.artwork.id)]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Artwork")
+        self.assertContains(response, "Test description")
+        self.assertContains(response, "10")
